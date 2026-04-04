53ccca3f71798c767e8d0145303cf8fa6d9bd298
Adrian Rodriguez <rodriguezcadr@gmail.com>
2026-04-04 18:39:44 -0300

    refactor dto validations

--- src/main/kotlin/com/ecommerce/presentation/dto/product/request/CreateProductReq.kt
+++ src/main/kotlin/com/ecommerce/presentation/dto/product/request/CreateProductReq.kt
@@ -1,5 +1,7 @@
 package com.ecommerce.presentation.dto.product.request
 
+import com.ecommerce.domain.error.ErrorHandler
+import com.ecommerce.domain.error.ErrorType
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Positive
@@ -18,82 +20,21 @@ data class CreateProductReq(
     @field:NotNull
     @field:Positive
     val price: Double,
-<<<<<<< Updated upstream
+
+    @field:Positive
+    val offerPrice: Double?,
 
     @field:NotNull
-=======
-    val offerPrice: Double?,
->>>>>>> Stashed changes
     val stock: Int,
 
     @field:NotBlank
     val categoryId: String,
 
-    val mainImage: String,
+    val mainImage: String?,
 
-    val images: List<String>,
-<<<<<<< Updated upstream
-=======
-    val isVisible: Boolean
-) {
-    companion object {
-        fun build(
-            token: String?,
-            name: String?,
-            description: String?,
-            price: Double?,
-            offerPrice: Double?,
-            stock: Int?,
-            categoryId: String?,
-            mainImage: String?,
-            images: List<String>?,
-            isVisible: Boolean?
-        ): CreateProductReq {
->>>>>>> Stashed changes
+    val images: List<String> = emptyList(),
 
     @field:NotNull
-    val isVisible: Boolean,
+    val isVisible: Boolean
 
-<<<<<<< Updated upstream
-)
-=======
-            if (name.isNullOrEmpty()) {
-                throw ErrorHandler(ErrorType.MISSING_REQUIRED_FIELDS)
-            }
-
-            if (description.isNullOrEmpty()) {
-                throw ErrorHandler(ErrorType.MISSING_REQUIRED_FIELDS)
-            }
-
-            if (categoryId.isNullOrEmpty()) {
-                throw ErrorHandler(ErrorType.MISSING_REQUIRED_FIELDS)
-            }
-
-            if (price == null) {
-                throw ErrorHandler(ErrorType.MISSING_REQUIRED_FIELDS)
-            }
-
-            if (stock == null) {
-                throw ErrorHandler(ErrorType.MISSING_REQUIRED_FIELDS)
-            }
-
-            if (isVisible == null) {
-                throw ErrorHandler(ErrorType.MISSING_REQUIRED_FIELDS)
-            }
-
-            return CreateProductReq(
-                token,
-                name,
-                description,
-                price,
-                offerPrice,
-                stock,
-                categoryId,
-                mainImage,
-                images ?: emptyList(),
-                isVisible
-            )
-        }
-    }
-}
->>>>>>> Stashed changes
+) {}
